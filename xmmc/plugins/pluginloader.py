cf2b6918b012cd2ac5a42d330e82debc3f15c67e
z <z@xnz.me>
2016-03-09 01:01:31 -0500

    use globally

--- xmmc/plugins/pluginloader.py
+++ xmmc/plugins/pluginloader.py
@@ -1,8 +1,8 @@
 import imp
 import os
 
-plugin_folder = "./xmmc/plugins"
-main_module = "__init__"
+plugin_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+main_module = '__init__'
 
 
 def get_plugins():
@@ -10,12 +10,12 @@ def get_plugins():
     possible_plugins = os.listdir(plugin_folder)
     for i in possible_plugins:
         location = os.path.join(plugin_folder, i)
-        if not os.path.isdir(location) or not main_module + ".py" in os.listdir(location):
+        if not os.path.isdir(location) or not main_module + '.py' in os.listdir(location):
             continue
         info = imp.find_module(main_module, [location])
-        plugins.append({"name": i, "info": info})
+        plugins.append({'name': i, 'info': info})
     return plugins
 
 
 def load_plugin(plugin):
-    return imp.load_module(plugin['name'], *plugin["info"])
+    return imp.load_module(plugin['name'], *plugin['info'])
