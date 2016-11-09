5b2f7001631370b8cdd7601ba2d06475e03dc02c
z <z@xnz.me>
2016-11-08 21:39:18 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup
 from setuptools import find_packages
-from xmm import __author__, __email__, __url__, __version__
+from xmm import __author__, __email__, __url__, __license__, __version__, __summary__, __keywords__
 
 
 with open('README.md') as f:
     readme_contents = f.read()
-
-with open('LICENSE') as f:
-    license_contents = f.read()
 
 with open('requirements.in') as f:
     install_requires = f.read().splitlines()
@@ -15,12 +12,12 @@ with open('requirements.in') as f:
 setup(
     name='xmm',
     version=__version__,
-    description='Xonotic Map Manager',
+    description=__summary__,
     long_description=readme_contents,
     author=__author__,
     author_email=__email__,
     url=__url__,
-    license=license_contents,
+    license=__license__,
     packages=find_packages(exclude=('tests', 'docs')),
     package_data={'': ['LICENSE', 'README.md', 'docs/*', 'config/*', 'bin/*', 'pkg/*', 'resources/*']},
     include_package_data=True,
@@ -33,4 +30,21 @@ setup(
           'xmm = xmm.cli:main'
        ]
     },
+    keywords=__keywords__,
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Development Status :: 4 - Beta',
+
+        'Environment :: Console',
+
+        'Topic :: Games/Entertainment',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
